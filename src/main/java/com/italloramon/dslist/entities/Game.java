a0eae5ce8196cb2d150fb084df34c235e1da707f
italloramon <irvp@ic.ufal.br>
2023-05-09 20:48:52 -0300

    Game seed

--- src/main/java/com/italloramon/dslist/entities/Game.java
+++ src/main/java/com/italloramon/dslist/entities/Game.java
@@ -14,20 +14,26 @@ public class Game {
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String platform;
+    private String platforms;
+
+    private Double score;
+    @Column(name = "img_url")
     private String imageUrl;
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game() {
     }
 
-    public Game(Long id, String title, Integer year, String genre, String platform, String imageUrl, String shortDescription, String longDescription) {
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imageUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.platform = platform;
+        this.platforms = platforms;
+        this.score = score;
         this.imageUrl = imageUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
@@ -65,12 +71,20 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlatform() {
-        return platform;
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public void setPlatform(String platform) {
-        this.platform = platform;
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
     }
 
     public String getImageUrl() {
