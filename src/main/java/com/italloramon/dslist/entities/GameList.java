aa72bf941d48aae3634279e183bc7154f17575f0
italloramon <irvp@ic.ufal.br>
2023-05-13 21:16:59 -0300

    Database seed update

--- src/main/java/com/italloramon/dslist/entities/GameList.java
+++ src/main/java/com/italloramon/dslist/entities/GameList.java
@@ -5,7 +5,7 @@ import jakarta.persistence.*;
 import java.util.Objects;
 
 @Entity
-@Table(name = "tb_gamelist")
+@Table(name = "tb_game_list")
 public class GameList {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
