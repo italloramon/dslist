5066cd254625ad1aaad1e7dce9419289d2d24796
italloramon <irvp@ic.ufal.br>
2023-05-14 21:57:28 -0300

    Homolog ok

--- src/main/java/com/italloramon/dslist/projections/GameMinProjection.java
+++ src/main/java/com/italloramon/dslist/projections/GameMinProjection.java
@@ -3,7 +3,7 @@ package com.italloramon.dslist.projections;
 public interface GameMinProjection {
     Long getId();
     String getTitle();
-    Integer getYear();
+    Integer getGameYear();
     String getImgUrl();
     String getShortDescription();
     Integer getPosition();
